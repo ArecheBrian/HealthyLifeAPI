fa529419a0fd0f1bdba59cc64233fc36f21e7d9f
Brian Javier Areche Minaya <areche@Brians-MacBook-Pro.local>
2023-09-10 18:32:18 -0400

    update

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('pn con queso');
-});
-
-module.exports = router;
