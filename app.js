b2a9f050713705529cccefef92242fd384e58de8
Brian Javier Areche Minaya <areche@Brians-MacBook-Pro.local>
2023-09-12 14:15:44 -0400

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var foodRouter = require('./routes/food');
+var alimentosRouter = require('./routes/alimentos');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/food', foodRouter);
+app.use('/api/alimentos', alimentosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
